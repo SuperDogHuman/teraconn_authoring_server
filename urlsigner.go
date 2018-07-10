963dcb3035379b0523da16ee2b4f2525af9e2be3
HironoriOnishi <onishi.hironori@gmail.com>
2018-07-10 11:23:06 +0900

    generate random filename in severside

--- urlsigner.go
+++ urlsigner.go
@@ -4,6 +4,7 @@ import (
   "bytes"
   "context"
   "encoding/json"
+  "github.com/rs/xid"
   "google.golang.org/appengine"
   "google.golang.org/appengine/file"
   "google.golang.org/cloud/storage"
@@ -12,18 +13,19 @@ import (
   "time"
 )
 
-var bucket string
 var ctx context.Context
-var filename string
+var bucket string
 
 func init() {
     http.HandleFunc("/", handler)
 }
 
+// ルーティング
+// ファイル名はこちら側で生成
+// GETからPUTに変更（何もなしでいきなりPUTできる？
 func handler(w http.ResponseWriter, r *http.Request) {
     ctx = appengine.NewContext(r)
     bucket, _ = file.DefaultBucketName(ctx)
-    filename = r.URL.Query()["filename"][0]
 
     w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusCreated)
@@ -32,7 +34,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func signedURL() []byte {
   account, _ := appengine.ServiceAccount(ctx)
-  expire := time.Now().AddDate(1, 0, 0)
+  expire     := time.Now().AddDate(1, 0, 0)
+  filename   := xid.New().String() + ".wav"
 
   url, sign_err := storage.SignedURL(bucket, filename, &storage.SignedURLOptions {
     GoogleAccessID: account,
@@ -48,9 +51,13 @@ func signedURL() []byte {
     log.Fatal(sign_err)
   }
 
-  response := Response{ SignedURL: url }
+  response := Response{ SignedURL: url, Filename: filename }
+  return responceToJson(response)
+}
 
+func responceToJson(response Response) []byte{
   buffer := bytes.NewBuffer([]byte{})
+
   jsonEncoder := json.NewEncoder(buffer)
   jsonEncoder.SetEscapeHTML(false)
   jsonEncoder.Encode(response)
@@ -59,5 +66,6 @@ func signedURL() []byte {
 }
 
 type Response struct {
+  Filename string `json:"filename"`
   SignedURL string `json:"signed_url"`
 }
