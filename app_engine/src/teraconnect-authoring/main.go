7cc391fde4b7a0e6991c832d159a2cd4d7f78d94
HironoriOnishi <onishi.hironori@gmail.com>
2018-07-29 23:37:28 +0900

    remove unnecessary codes in authoring endpoints

--- app_engine/src/teraconnect-authoring/main.go
+++ app_engine/src/teraconnect-authoring/main.go
@@ -1,18 +1,2 @@
 package main
-
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-)
-
-func init() {
-	e := echo.New()
-	e.Pre(middleware.RemoveTrailingSlash())
-	e.Use(middleware.CORS())
-
-	//	e.GET("/lessons/:id", getLesson)
-
-	http.Handle("/", e)
-}
+func init() {}
