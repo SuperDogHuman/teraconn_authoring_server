5a7927bb800bceb7862fad2ba67b817ec8088afe
HironoriOnishi <onishi.hironori@gmail.com>
2018-09-04 02:47:46 +0900

    added down sampling wav files function, support node.js 8

--- cloud_functions/index.js
+++ cloud_functions/index.js
@@ -4,144 +4,179 @@ const ffmpegPath = require('ffmpeg-static').path;
 const storage    = require('@google-cloud/storage')();
 const datastore  = require('@google-cloud/datastore')();
 
-exports.wavToText = (event, callback) => {
-  const file = event.data;
+exports.wavToText = async (data, context) => {
+    const file = data;
+    const request = {
+        audio: {
+            uri: `gs://${file.bucket}/${file.name}`
+        },
+        config: {
+            encoding:        'LINEAR16',
+            sampleRateHertz: 16000,
+            languageCode:    'ja-JP',
+        },
+    };
 
-  if (file.size == 0) { return callback(); }
+    const speech = require('@google-cloud/speech');
+    const client = new speech.v1.SpeechClient();
+    const responses = await client.recognize(request).catch((err) => {
+        console.error(err);
+    });
 
-  const request = {
-    audio: {
-      uri: `gs://${file.bucket}/${file.name}`
-    },
-    config: {
-      encoding:        'LINEAR16',
-      sampleRateHertz: 16000,
-      languageCode:    'ja-JP',
-    },
-  };
-
-  const speech = require('@google-cloud/speech');
-  const client = new speech.v1.SpeechClient();
-
-  client.recognize(request)
-    .then(data => {
-      const response = data[0];
-      const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
+    const response = responses[0];
+    const transcription = response.results
+        .map((result) => result.alternatives[0].transcript)
         .join('\n');
 
-      const fileNames = file.name.split('-');
-      const lessonID  = fileNames[0];
-      const fileID    = fileNames[1].slice(0, -4);
-      const info = {
+    const fileNames = file.name.split('-');
+    const lessonID  = fileNames[0];
+    const fileID    = fileNames[1].slice(0, -4);
+    const info = {
         LessonID: lessonID,
         FileID:   fileID,
         IsTexted: true,
         Text:     transcription,
-      };
-      recordVoiceInfo(fileID, info)
-    })
-    .then(() => { callback(); })
-    .catch((err) => {
-      console.error(err);
-      callback();
-    });
+    };
+    recordVoiceInfo(fileID, info);
 }
 
-exports.wavToOgg = (event, callback) => {
-  const file = event.data;
-  if (file.size == 0) { return callback(); }
+exports.wavTo16Khz = async (data, context) => {
+    const file = data;
+    if (file.size == 0) { return; }
 
-  const fileNames         = file.name.split('-');
-  const lessonID          = fileNames[0];
-  const fileID            = fileNames[1].slice(0, -4);
-  const localWAVFilePath  = `/tmp/${fileID}.wav`;
-  const localOGGFilePath  = `/tmp/${fileID}.ogg`;
-  const remoteOGGFilePath = `voice/${lessonID}/${fileID}.ogg`;
+    const fileNames                 = file.name.split('-');
+    const fileID                    = fileNames[1].slice(0, -4);
+    const localRawWAVFilePath       = `/tmp/raw-${fileID}.wav`;
+    const localResampledWAVFilePath = `/tmp/resampled-${fileID}.wav`;
+    const remoteWAVFilePath         = file.name;
 
-  downloadFromStorage(file.name, localWAVFilePath)
-    .then(() => {
-      return encodeToOgg(localWAVFilePath, localOGGFilePath);
-    })
-    .then(() => {
-      return uploadToStorage(localOGGFilePath, remoteOGGFilePath);
-    })
-    .then(() => {
-      const info = {
+    await downloadFromStorage('teraconn_raw_voice', file.name, localRawWAVFilePath).catch((err) => {
+        console.error(err);
+    });
+
+    await encodeToWav(localRawWAVFilePath, localResampledWAVFilePath).catch((err) => {
+        console.error(err);
+    });
+
+    await uploadToStorage('teraconn_voice_for_transcription', localResampledWAVFilePath, remoteWAVFilePath).catch((err) => {
+        console.error(err);
+    });
+
+    function encodeToWav(rawFilePath, resampledFilePath) {
+        return new Promise((resolve, reject) => {
+            ffmpeg(rawFilePath)
+                .setFfmpegPath(ffmpegPath)
+                .audioChannels(1)
+                .audioFrequency(16000)
+                .output(resampledFilePath)
+                .on('end', () => { resolve(); })
+                .on('error', (err) => {
+                    console.error(err);
+                    reject(error);
+                })
+                .run();
+        });
+    }
+}
+
+exports.wavToOgg = async (data, context) => {
+    const file = data;
+    if (file.size == 0) { return; }
+
+    const fileNames         = file.name.split('-');
+    const lessonID          = fileNames[0];
+    const fileID            = fileNames[1].slice(0, -4);
+    const localWAVFilePath  = `/tmp/${fileID}.wav`;
+    const localOGGFilePath  = `/tmp/${fileID}.ogg`;
+    const remoteOGGFilePath = `voice/${lessonID}/${fileID}.ogg`;
+
+    await downloadFromStorage('teraconn_raw_voice', file.name, localWAVFilePath).catch((err) => {
+        console.error(err);
+    });
+
+    await encodeToOgg(localWAVFilePath, localOGGFilePath).catch((err) => {
+        console.error(err);
+    });
+
+    await uploadToStorage('teraconn_material', localOGGFilePath, remoteOGGFilePath).catch((err) => {
+        console.error(err);
+    });
+
+    const info = {
         LessonID:    lessonID,
         FileID:      fileID,
         IsConverted: true,
-      };
-      return recordVoiceInfo(fileID, info);
-    })
-    .then(() => { callback(); })
-    .catch((err) => {
-      console.error(err);
-      callback();
+    };
+    await recordVoiceInfo(fileID, info).catch((err) => {
+        console.error(err);
     });
 
-  function downloadFromStorage(remoteFilePath, localFilePath) {
-    return storage.bucket('teraconn_raw_voice')
-      .file(remoteFilePath)
-      .download({ destination: localFilePath });
-  }
+    function encodeToOgg(wavFilePath, oggFilePath) {
+        return new Promise((resolve, reject) => {
+            ffmpeg(wavFilePath)
+                .setFfmpegPath(ffmpegPath)
+                .audioCodec('libvorbis')
+                .audioQuality(0)
+                .audioChannels(1)
+//                .audioFilters(
+//                    [{ filter: 'dynaudnorm', options: '' }]
+//                )
+                .format('ogg')
+                .output(oggFilePath)
+                .on('end', () => { resolve(); })
+                .on('error', (err) => {
+                    console.error(err);
+                    reject(error);
+                })
+            .run();
+        });
+    }
+}
 
-  function encodeToOgg(wavFilePath, oggFilePath) {
-    return new Promise((resolve, reject) => {
-      ffmpeg(wavFilePath)
-        .setFfmpegPath(ffmpegPath)
-        .audioChannels(1)
-        .audioFrequency(16000)
-        .format('ogg')
-        .output(oggFilePath)
-        .on('end', () => { resolve(); })
-        .on('error', (err) => {
-          console.error(err);
-          reject(error);
-        })
-        .run();
-    });
-  }
+function downloadFromStorage(bucketName, remoteFilePath, localFilePath) {
+    return storage.bucket(bucketName)
+        .file(remoteFilePath)
+        .download({ destination: localFilePath });
+}
 
-  function uploadToStorage(localFilePath, remoteFilePath) {
-    const bucket = storage.bucket('teraconn_material');
+function uploadToStorage(bucketName, localFilePath, remoteFilePath) {
+    const bucket = storage.bucket(bucketName);
     const options = {
-      destination: bucket.file(remoteFilePath),
-      resumable:   false,
+        destination: bucket.file(remoteFilePath),
+        resumable:   false,
     };
     return bucket.upload(localFilePath, options);
-  }
 }
 
 function recordVoiceInfo(fileID, info) {
-  const transaction = datastore.transaction();
-  const key         = datastore.key(['VoiceText', fileID]);
-  return transaction.run()
-    .then(function(data) {
-      return transaction.get(key);
-    })
-    .then((results) => {
-      let entity = results[0];
-      if (entity) {
-        Object.keys(info).forEach((k) => {
-          entity[k] = info[k];
+    const transaction = datastore.transaction();
+    const key         = datastore.key(['VoiceText', fileID]);
+    return transaction.run()
+        .then((data) => {
+            return transaction.get(key);
+        })
+        .then((results) => {
+            let entity = results[0];
+            if (entity) {
+            Object.keys(info).forEach((k) => {
+                entity[k] = info[k];
+            });
+            return transaction.save({ key: key, data: entity });
+            } else {
+                entity = { key: key, data: info};
+                return transaction.save(entity);
+            }
+        })
+        .then(() =>{
+            return transaction.commit();
+        })
+        .catch((err) => {
+            if (err.code == 10) {
+                console.warn("too much contention, retry after 1 second");
+                return setTimeout(recordVoiceInfo.bind(this, fileID, info), 1000);
+            } else {
+                    console.error(err);
+                    return transaction.rollback();
+            }
         });
-        return transaction.save({ key: key, data: entity });
-      } else {
-        entity = { key: key, data: info};
-        return transaction.save(entity);
-      }
-    })
-    .then(() =>{
-      return transaction.commit();
-    })
-    .catch((err) => {
-      if (err.code == 10) {
-        console.warn("too much contention, retry after 1 second");
-        return setTimeout(recordVoiceInfo.bind(this, fileID, info), 1000);
-      } else {
-        console.error(err);
-        return transaction.rollback();
-      }
-    });
 }
