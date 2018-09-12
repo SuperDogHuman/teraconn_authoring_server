b8d75125a75b4a92415e5d8fd6e878f24b5c9864
HironoriOnishi <onishi.hironori@gmail.com>
2018-09-13 01:41:10 +0900

    remove unused code in graphic

--- app_engine/src/graphic/graphic.go
+++ app_engine/src/graphic/graphic.go
@@ -2,54 +2,11 @@ package graphic
 
 import (
 	"github.com/labstack/echo"
-	"github.com/pkg/errors"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/datastore"
-	"google.golang.org/appengine/log"
 
-	"cloudHelper"
-	"lessonType"
 	"net/http"
-	"utility"
 )
 
 // Gets is get lesson graphic.
 func Gets(c echo.Context) error {
-	ctx := appengine.NewContext(c.Request())
-	id := c.Param("id")
-
-	ids := []string{id}
-	if !utility.IsValidXIDs(ids) {
-		errMessage := "Invalid ID(s) error"
-		log.Warningf(ctx, errMessage)
-		return c.JSON(http.StatusBadRequest, errMessage)
-	}
-
-	lessonGraphic := new(lessonType.LessonGraphic)
-	lessonGraphic.ID = id // LessonGraphicID is the same as lessonID
-
-	var err error
-	if err = cloudHelper.FetchEntityFromGCD(ctx, lessonGraphic, "LessonGraphic"); err != nil {
-		log.Errorf(ctx, "%+v\n", errors.WithStack(err))
-		if err == datastore.ErrNoSuchEntity {
-			return c.JSON(http.StatusNotFound, err.Error())
-		}
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-
-	var keys []*datastore.Key
-	for _, id := range lessonGraphic.GraphicIDs {
-		keys = append(keys, datastore.NewKey(ctx, "Graphic", id, 0, nil))
-	}
-
-	graphicCount := len(lessonGraphic.GraphicIDs)
-	graphics := make([]lessonType.Graphic, graphicCount)
-	if err = datastore.GetMulti(ctx, keys, graphics); err != nil {
-		log.Errorf(ctx, "%+v\n", errors.WithStack(err))
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-
-	lessonGraphic.Graphics = graphics
-
-	return c.JSON(http.StatusOK, lessonGraphic)
+	return c.JSON(http.StatusOK, "dummy")
 }
